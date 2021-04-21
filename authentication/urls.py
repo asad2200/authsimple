2c93baec110aff7a04720a73cf173a1f52b031d4
asad2200 <asadjakhavala92@gmail.com>
2021-04-21 16:38:00 +0530

    make changes in urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('verify/<uuid:code>', views.verify, name='verify'),
-    path('verify/new', views.verify_new, name='verify_new'),
+    path('verify/new/', views.verify_new, name='verify_new'),
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('reset_password/<uuid:code>',
          views.reset_password, name='reset_password'),
